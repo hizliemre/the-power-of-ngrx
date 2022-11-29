9cac022e55d2aee1ecd9c7c19f637d56bb345ec3
Emre Hızlı <hizliemre@gmail.com>
2022-11-30 01:35:21 +0300

    login box

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
-  standalone: true
+  standalone: true,
+  imports: [NgClass],
+  styles: [`:host { @apply flex-1 flex; }`]
 })
 export default class LoginComponent implements OnInit {
+
+  showPassword = false;
+
   constructor() { }
 
   ngOnInit() { }
